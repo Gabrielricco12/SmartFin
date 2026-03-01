7c84f99b330137394c80cd822a41a65dde6e0d7a
Gabrielricco12 <gabrielricco2023@gmail.com>
2026-03-01 11:32:33 -0300

    Delete src directory

--- src/stores/useRefreshStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-export const useRefreshStore = create((set) => ({
-  refreshKey: 0,
-  triggerRefresh: () => set((state) => ({ refreshKey: state.refreshKey + 1 })),
-}));
--- src/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-import { getHours } from 'date-fns';
-
-export function getGreeting() {
-  const currentHour = getHours(new Date());
-  
-  if (currentHour >= 5 && currentHour < 12) return 'Bom dia';
-  if (currentHour >= 12 && currentHour < 18) return 'Boa tarde';
-  return 'Boa noite';
-}
